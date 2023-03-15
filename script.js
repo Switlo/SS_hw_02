c4248f6710bc67a1513a11ab50aed1d14160cec9
Svitlana R <sv73@ukr.net>
2023-03-15 12:02:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 // NORMAL
 // 1
-const height = prompt(`Для того, щоб визначити об'єм вашого циліндра, введіть, будь ласка, його висоту у см`);
-const radius = prompt(`Тепер введіть радіус у см`);
-const cylinderVolume = Math.round(3.1415 * Math.pow(radius, 2) * height);
+const height = +prompt(`Для того, щоб визначити об'єм вашого циліндра, введіть, будь ласка, його висоту у см`);
+const radius = +prompt(`Тепер введіть радіус у см`);
+const cylinderVolume = Math.round(3.1415 * Math.pow(radius, 2) * height); // Math.PI - це замість 3.1415 (пі)
 alert(`Об'єм вашого циліндру — ${cylinderVolume} см³.`);
 
 // 2
@@ -12,18 +12,18 @@ const userName = prompt(`Напишіть, будь ласка, своє ім'я
 alert(`Привіт, ${userName}!`);
 
 // 3
-const sideLength = prompt(`Введіть, будь ласка, довжину сторони квадрата у см`); 
+const sideLength = +prompt(`Введіть, будь ласка, довжину сторони квадрата у см`); 
 alert(`Периметр вашого квадрата — ${sideLength * 4} см.`);
 
 // 4
-const distance = prompt(`Введіть, будь ласка, відстань у км, яку потрібно подолати`);
-const time = prompt(`За скільки годин вам потрібно подолати цю відстань?`);
+const distance = +prompt(`Введіть, будь ласка, відстань у км, яку потрібно подолати`);
+const time = +prompt(`За скільки годин вам потрібно подолати цю відстань?`);
 alert(`Щоб досягти мети, вам слід рухатись зі швидкістю ${Math.round(distance / time)} км/год`);
 
 // HARD
 // 1
 const userNumber = prompt(`Введіть тризначне число`);
-alert(`Отримайте його "перевертень" — ${userNumber.split("").reverse().join("")}.`);
+alert(`Отримайте його "перевертень" — ${userNumber.split("").reverse().join("")}.`); // можна розв'язувати через slice() та ділення %
 
 // split - розбиває рядок на символи;
 // reverse - перевертає массив;
@@ -31,7 +31,7 @@ alert(`Отримайте його "перевертень" — ${userNumber.spl
 
 
 // 2
-const celsiusTemperature = prompt(`Cavin, type please a temperature in Celsius`);
+const celsiusTemperature = +prompt(`Cavin, type please a temperature in Celsius`);
 alert(`The temperature in Fahrenheit is ${Math.round((celsiusTemperature * 9) / 5 + 32)}.`);
 
 // 3
